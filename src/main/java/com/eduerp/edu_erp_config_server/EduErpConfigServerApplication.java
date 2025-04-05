9cb34e1bf6571a30f05bef55a306d0faf32cc4bc
gjaku1031 <gjaku1031@gmail.com>
2025-04-05 21:27:15 +0900

    first commit

---
+++ src/main/java/com/eduerp/edu_erp_config_server/EduErpConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.eduerp.edu_erp_config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduErpConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduErpConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eduerp/edu_erp_config_server/EduErpConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduerp.edu_erp_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduErpConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
