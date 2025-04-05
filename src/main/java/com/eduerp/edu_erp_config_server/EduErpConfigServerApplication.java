f70edee7a4722739d1f2977c5d61d1697848d13f
gjaku1031 <gjaku1031@gmail.com>
2025-04-05 23:02:28 +0900

    Spring Boot 보안 스타터 및 클라우드 설정 추가, 애플리케이션 구성 파일 업데이트

--- src/main/java/com/eduerp/edu_erp_config_server/EduErpConfigServerApplication.java
+++ src/main/java/com/eduerp/edu_erp_config_server/EduErpConfigServerApplication.java
@@ -2,12 +2,13 @@ package com.eduerp.edu_erp_config_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer//config Server를 위한 애노테이션
 public class EduErpConfigServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EduErpConfigServerApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/eduerp/edu_erp_config_server/security/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.eduerp.edu_erp_config_server.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf((auth) -> auth.disable());
+
+        http
+                .authorizeHttpRequests((auth) -> auth.anyRequest().authenticated());
+
+        http
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
