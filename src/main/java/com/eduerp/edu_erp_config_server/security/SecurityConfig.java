d387fd7e498e4ae1685b287c367336fa63a3c386
gjaku1031 <gjaku1031@gmail.com>
2025-04-06 14:42:04 +0900

    config server 설정

--- src/main/java/com/eduerp/edu_erp_config_server/security/SecurityConfig.java
+++ src/main/java/com/eduerp/edu_erp_config_server/security/SecurityConfig.java
@@ -5,7 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -30,4 +34,15 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+    @Bean //test용 admin 유저
+    public UserDetailsService userDetailsService() {
+        UserDetails user1 = User.builder()
+                .username("admin")
+                .password(bCryptPasswordEncoder().encode("1234"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user1);
+    }
+
 }
